060e992d816a2f5c34338e0f4df14b0a40d125f0
thomasdenecker <thomas.denecker@gmail.com>
2023-08-08 16:01:43 +0200

    feat: create a community page

---
+++ pages/community.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Community</h1>
+  <p>This site is currently under development. We do not yet have any users. If you are interested, please contact us!</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
