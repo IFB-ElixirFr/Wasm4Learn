bdc2bf37785f2b976cd30e3a24c3ac41b507ec9b
thomasdenecker <thomas.denecker@gmail.com>
2023-08-14 17:16:01 +0200

    feat: Create belt component

---
+++ components/Betls.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-chip
+    v-for="(b, kb) in belts"
+    :key="kb"
+    class="ma-1 rounded-sm"
+    size="small"
+    :variant="b == 'white' ? 'outlined' : 'tonal'"
+    :color="b == 'white' ? '' : b"
+  >
+    {{ b }} belt
+  </v-chip>
+</template>
+
+<script>
+export default {
+  props: {
+    belts: [],
+  },
+};
+</script>
+
+<style>
+</style>
